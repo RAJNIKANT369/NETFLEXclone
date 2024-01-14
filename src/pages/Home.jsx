d0da7d3c3937cc49b13f15c09a098e55691a873e
RAJNIKANT369 <133694974+RAJNIKANT369@users.noreply.github.com>
2024-01-14 14:43:17 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TabComponent from '../components/Tabs'
+import Login from './Login'
+
+const Home = () => {
+  return (
+    <>
+      <Login/>
+      <TabComponent />
+    </>
+  )
+}
+
+export default Home
